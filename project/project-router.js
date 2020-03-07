e6cc27d2c05bbe48b2e8a28bb93914e8c4b0a7e0
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 17:55:47 -0800

    project-router > .post(), created

--- project/project-router.js
+++ project/project-router.js
@@ -7,11 +7,29 @@ const router = express.Router({
 })
 
 router.get("/", (req, res) => {
-
+    Project.find()
+    .then(project => {
+      res.json(project);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Failed to return Projects. Good-bye." });
+    });
 })
 
 router.post("/", (req, res) => {
-
+    const projectData = req.body;
+  
+    Project.add(projectData)
+      .then(project => {
+        res.status(201).json(project);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: "Failed to create new Project. Good-luck." });
+      });
 })
 
 module.exports = router;
