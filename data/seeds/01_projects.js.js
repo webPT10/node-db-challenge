855a22cb830286a8050454090c475276d36190a9
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 17:33:30 -0800

    seeds > 01_projects, created

--- data/seeds/01_projects.js.js
+++ data/seeds/01_projects.js.js
@@ -1,13 +1,7 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function(knex) {
+  await knex("projects").insert([
+    { name: "Plant Tree", description: "Plant a tree in backyard.", completed: false },
+    { name: "Widdle", description: "Widdle with wood.", completed: false },
+])
 };
