de89e283ef645e313559e1104aab973810c6dcc9
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 18:05:05 -0800

    task-model >> findById, add, created

--- task/task-model.js
+++ task/task-model.js
@@ -1,4 +1,27 @@
 const db = require('../data/config')
 
+// find by id
+function findById(id) {
+    return db("tasks")
+      .where({ "tasks.id": id })
+      .first();
+  }
+  
+  // add tasks
+  // INSERT INTO tasks(name)
+  // VALUES ("Put up photo.");
+  function add(task) {
+    return db("tasks")
+      .insert(task)
+      .then(ids => {
+        return findById(ids);
+      });
+  }
 
-module.exports = {}
+
+
+module.exports = {
+    findById,
+    add,
+    find
+}
