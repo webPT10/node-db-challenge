afa906f21b3a842dd95be8a1fdfed1ecc66a6128
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 18:10:39 -0800

    task-router > .put(), created

--- task/task-router.js
+++ task/task-router.js
@@ -17,7 +17,17 @@ router.get("/", (req, res) => {
 })
 
 router.post("/", (req, res) => {
+    const taskData = req.body;
 
+    Task.add(taskData)
+      .then(task => {
+        res.status(201).json(task);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: "Failed to create new Task. Good-luck." });
+      });
 })
 
 module.exports = router;
