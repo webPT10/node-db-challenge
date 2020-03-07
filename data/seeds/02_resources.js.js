36b6c9f9577945d098b4e4a812caa7fc20df00af
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 17:35:50 -0800

    seeds > 02_resources.js, created

--- data/seeds/02_resources.js.js
+++ data/seeds/02_resources.js.js
@@ -1,3 +1,10 @@
 
 exports.seed = async function(knex) {
+    await knex("resources").insert([
+      { name: "knife", description: "4'inch blade, very sharp." },
+      { name: "stool", description: "Gardening Stool." },
+      { name: "shovel", description: "6 in. W x 47 in. L Drain Spade." },
+      { name: "seed grow kit", description: "Giant Sequoia." },
+      { name: "wood", description: "A block of wood." },
+  ])
 };
