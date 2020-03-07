884dec71aa84244ece0034954cc24ba7d3bc37c2
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 17:38:19 -0800

    seeds > 03_tasks.js, created

--- data/seeds/03_tasks.js.js
+++ data/seeds/03_tasks.js.js
@@ -1,3 +1,13 @@
 
 exports.seed = async function(knex) {
+  await knex("tasks").insert([
+    { description: "Go outside", notes: "Open the door, walk-outside.", completed: false },
+    { description: "Sit down in stool.", notes: "Sit in stool.", completed: false },
+    { description: "Pull out knife.", notes: "Remove knife from carry case.", completed: false },
+    { description: "Pickup block of wood", completed: false },
+    { description: "Begin to widdle.", notes: "You will now be widdling.", completed: false },
+    { description: "Pick up the shovel.", notes: "Its still in the garden.", completed: false },
+    { description: "Dig a hole in the ground.", notes: "About 1.5ft deep.", completed: false },
+    { description: "Follow directions on tree planting seed kit.", completed: false },    
+])
 };
