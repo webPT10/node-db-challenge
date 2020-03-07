b6767a11a0e66d2a283de6a7f50387b959fd4522
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 18:00:30 -0800

    resource-model > findById(), add(), created

--- resource/resource-model.js
+++ resource/resource-model.js
@@ -1,5 +1,22 @@
 const db = require('../data/config')
 
+// find by id
+function findById(id) {
+    return db("resources")
+      .where({ "resources.id": id })
+      .first()
+  }
+  
+  // add resources
+  // INSERT INTO resources(name)
+  // VALUES ("hammer");
+  function add(resource) {
+    return db("resources")
+      .insert(resource)
+      .then(ids => {
+        return findById(ids);
+      });
+  }
 
 // retrieve a list of resources
 function find() {
@@ -7,5 +24,7 @@ function find() {
   }
 
 module.exports = {
-    find,
+    findById,
+    add, 
+    find
 }
