7148325c226f915798df4144ff0b4d1cbe0d0447
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-06 17:39:35 -0800

    seeds > 04_projects_resources.js, created >> spacing fixed on 01

--- data/seeds/01_projects.js.js
+++ data/seeds/01_projects.js.js
@@ -1,7 +1,7 @@
 
 exports.seed = async function(knex) {
-  await knex("projects").insert([
-    { name: "Plant Tree", description: "Plant a tree in backyard.", completed: false },
-    { name: "Widdle", description: "Widdle with wood.", completed: false },
-])
+    await knex("projects").insert([
+      { name: "Plant Tree", description: "Plant a tree in backyard.", completed: false },
+      { name: "Widdle", description: "Widdle with wood.", completed: false },
+  ])
 };
